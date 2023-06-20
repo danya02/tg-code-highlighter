0fa7ac9e829d13f77fad0172e6539f9a9f61e2ed
Danya Generalov <danya@danya02.ru>
2023-06-20 12:12:02 +0300

    Changing theme and some text.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{
     env,
-    time::{UNIX_EPOCH, SystemTime},
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 use cosmic_text::{FontSystem, SwashCache};
 use futures::StreamExt;
 use rand::{distributions, thread_rng, Rng};
 use sqlx::{query, SqlitePool};
-use syntect::{parsing::SyntaxSet, highlighting::ThemeSet};
+use syntect::{highlighting::ThemeSet, parsing::SyntaxSet};
 use telegram_bot::*;
 
 mod render;
@@ -26,6 +26,8 @@ struct Config {
     null_chat_id: i64,
 }
 
+const UNUSED_RESULT_ID: &str = "unused-result-id";
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     #[allow(unused_must_use)]
@@ -42,55 +44,90 @@ async fn main() -> anyhow::Result<()> {
     let swash_cache = SwashCache::new();
     let syntax_set = SyntaxSet::load_defaults_newlines();
     let theme_set = ThemeSet::load_defaults();
-    let config = Config{ null_chat_id: -992674722 }; // TODO: accept this from outside 
-    let mut state = State{api, pool, font_system, swash_cache, config, syntax_set, theme_set};
-
+    let config = Config {
+        null_chat_id: -992674722,
+    }; // TODO: accept this from outside
+    let mut state = State {
+        api,
+        pool,
+        font_system,
+        swash_cache,
+        config,
+        syntax_set,
+        theme_set,
+    };
 
     sqlx::migrate!().run(&state.pool).await?;
 
     // Fetch new updates via long poll method
     let mut stream = state.api.stream();
     while let Some(update) = stream.next().await {
-        // If the received update contains a new message...
         let update = update?;
-        match update.kind {
-            UpdateKind::Message(message) => {
-                if let MessageKind::Text { ref data, .. } = message.kind {
-                    // Print received text message to stdout.
-                    println!("<{}>: {}", &message.from.first_name, data);
-
-                    // Answer message with "Hi".
-                    state.api.send(message.text_reply(format!(
-                        "Hi, {}! You just wrote '{}' in chat {}",
-                        &message.from.first_name, data, message.chat.id()
-                    )))
-                    .await?;
-                }
-            }
-            UpdateKind::InlineQuery(query) => {
-                process_inline_query(&mut state, query).await?;
-            }
-            UpdateKind::ChosenInlineResult(chosen_inline_result) => {
-                // Once an inline result has been chosen, we need to make the gist corresponding to it not ephemeral.
-                query!("UPDATE gist SET is_ephemeral=0 WHERE id=?", chosen_inline_result.result_id).execute(&state.pool).await?;
-
-                // Also remove the expired ephemeral gists
-                let delete_cutoff = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() as i64 - 1800; // 30 minutes
-                query!("DELETE FROM gist WHERE is_ephemeral>0 AND sent_at_unix_time<?", delete_cutoff).execute(&state.pool).await?;
-            }
-            _ => {}
+        if let Err(e) = process_update(update, &mut state).await {
+            log::error!("Error while processing update: {e}");
         }
     }
+    Ok(())
+}
+
+async fn process_update(update: Update, state: &mut State) -> anyhow::Result<()> {
+    // If the received update contains a new message...
+    match update.kind {
+        UpdateKind::Message(message) => {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                // Print received text message to stdout.
+                println!("<{}>: {}", &message.from.first_name, data);
+
+                // Answer message with "Hi".
+                state
+                    .api
+                    .send(message.text_reply(format!(
+                        "Hi, {}! You just wrote '{}' in chat {}",
+                        &message.from.first_name,
+                        data,
+                        message.chat.id()
+                    )))
+                    .await?;
+            }
+        }
+        UpdateKind::InlineQuery(query) => {
+            process_inline_query(state, query).await?;
+        }
+        UpdateKind::ChosenInlineResult(chosen_inline_result) => {
+            if chosen_inline_result.result_id == UNUSED_RESULT_ID {
+                return Ok(());
+            }
+            // Once an inline result has been chosen, we need to make the gist corresponding to it not ephemeral.
+            query!(
+                "UPDATE gist SET is_ephemeral=0 WHERE id=?",
+                chosen_inline_result.result_id
+            )
+            .execute(&state.pool)
+            .await?;
+
+            // Also remove the expired ephemeral gists
+            let delete_cutoff = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as i64
+                - 1800; // 30 minutes
+            query!(
+                "DELETE FROM gist WHERE is_ephemeral>0 AND sent_at_unix_time<?",
+                delete_cutoff
+            )
+            .execute(&state.pool)
+            .await?;
+        }
+        _ => {}
+    }
+
     Ok(())
 }
 
 /// Process an inline query: save the code snippet as an ephemeral gist,
 /// then render it as an image
 /// and submit this as an inline query result.
-async fn process_inline_query(
-    state: &mut State,
-    inline_query: InlineQuery,
-) -> anyhow::Result<()> {
+async fn process_inline_query(state: &mut State, inline_query: InlineQuery) -> anyhow::Result<()> {
     let api = &state.api;
     let pool = &state.pool;
     let font_system = &mut state.font_system;
@@ -113,10 +150,40 @@ async fn process_inline_query(
 
     let code = if let Some(real_code) = real_code {
         real_code
-    } else {code.clone()};
+    } else {
+        code.clone()
+    };
 
-    let syntax = 
-    if let Some(ext) = code_ext {
+    // If the user did not type any code yet, then show an invitation to type some code, and do not render the empty code.
+    if code.trim().is_empty() {
+        api.send(
+            inline_query.answer(vec![InlineQueryResult::InlineQueryResultArticle(
+                InlineQueryResultArticle {
+                    id: UNUSED_RESULT_ID.to_string(),
+                    title: "Type some code to highlight!".to_string(),
+                    description: Some("try: py:print('Hello World')".to_string()),
+                    input_message_content: InputMessageContent::InputTextMessageContent(
+                        InputTextMessageContent {
+                            message_text: r"Welcome to the code highlighter bot\! To highlight some code, type it in the inline query box\. For example: `py:print\('Hello World'\)` and `cpp:int main\(int argc, char **argv\);`".to_string(),
+                            parse_mode: Some(ParseMode::MarkdownV2),
+                            disable_web_page_preview: true,
+                        },
+                    ),
+                    reply_markup: None,
+                    url: None,
+                    hide_url: true,
+                    thumb_url: None,
+                    thumb_width: None,
+                    thumb_height: None,
+                },
+            )]),
+        )
+        .await?;
+
+        return Ok(());
+    }
+
+    let syntax = if let Some(ext) = code_ext {
         ps.find_syntax_by_extension(ext)
     } else {
         Some(ps.find_syntax_plain_text())
@@ -153,41 +220,70 @@ async fn process_inline_query(
     // Set this to a chat that you control.
     // This will yield a server file_id, which can be then used in the inline query result photo.
 
-    let png_data = render::draw_code(font_system, swash_cache, ps, ts, &code, syntax.unwrap_or(ps.find_syntax_plain_text()));
+    let png_data = render::draw_code(
+        font_system,
+        swash_cache,
+        ps,
+        ts,
+        &code,
+        syntax.unwrap_or(ps.find_syntax_plain_text()),
+    );
 
     let photo_upload = InputFileUpload::with_data(png_data, "code.png");
 
-    let upload = api.send(SendPhoto::new(ChatId::from(config.null_chat_id), photo_upload)).await?;
+    let upload = api
+        .send(SendPhoto::new(
+            ChatId::from(config.null_chat_id),
+            photo_upload,
+        ))
+        .await?;
     let file_id = if let MessageKind::Photo { data, .. } = upload.kind {
         let first = data[0].clone();
-        let largest = data.iter().fold(first, |acc, item| if (acc.width, acc.height)<(item.width, item.height) {item.clone()} else {acc});
+        let largest = data.iter().fold(first, |acc, item| {
+            if (acc.width, acc.height) < (item.width, item.height) {
+                item.clone()
+            } else {
+                acc
+            }
+        });
         largest.file_id
     } else {
-        return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("Uploaded image with photo but it wasn't a photo message?!")))?;
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            format!("Uploaded image with photo but it wasn't a photo message?!"),
+        ))?;
     };
 
     let language = if code_ext.is_some() {
-        format!("Language: `{}`{}", code_ext.unwrap(),
-        if let None = syntax {r" \(ERROR: could not find syntax by this extension\!\)"} else {""})
+        format!(
+            "Language: `{}`{}",
+            code_ext.unwrap(),
+            if let None = syntax {
+                r" \(ERROR: could not find syntax by this extension\!\)"
+            } else {
+                ""
+            }
+        )
     } else {
-        format!(r"Language unknown \(try `py:print\('Hello World'\)` and `cpp:int main\(int argc, char **argv\);`\)")
+        format!(
+            r"Language unknown \(try `py:print\('Hello World'\)` and `cpp:int main\(int argc, char **argv\);`\)"
+        )
     };
 
-    api
-        .send(
-            inline_query.answer(vec![InlineQueryResult::InlineQueryResultCachedPhoto(
-                InlineQueryResultCachedPhoto {
-                    id: id.clone(),
-                    photo_file_id: file_id,
-                    title: None,
-                    description: None,
-                    caption: Some(format!("Code snippet ID: `{id}` {}", language)),
-                    parse_mode: Some(ParseMode::MarkdownV2),
-                    reply_markup: None,
-                    input_message_content: None,
-                },
-            )]),
-        )
-        .await?;
+    api.send(
+        inline_query.answer(vec![InlineQueryResult::InlineQueryResultCachedPhoto(
+            InlineQueryResultCachedPhoto {
+                id: id.clone(),
+                photo_file_id: file_id,
+                title: None,
+                description: None,
+                caption: Some(format!("Code snippet ID: `{id}` {}", language)),
+                parse_mode: Some(ParseMode::MarkdownV2),
+                reply_markup: None,
+                input_message_content: None,
+            },
+        )]),
+    )
+    .await?;
     Ok(())
 }
--- src/render.rs
+++ src/render.rs
@@ -1,38 +1,64 @@
 use std::io::Cursor;
 
-use cosmic_text::{SwashCache, FontSystem, Metrics, Attrs, Buffer, Color, Family, BufferLine, AttrsList};
-use image::{ImageBuffer, Rgba, ImageOutputFormat};
+use cosmic_text::{
+    Attrs, AttrsList, Buffer, BufferLine, Color, Family, FontSystem, Metrics, SwashCache,
+};
+use image::{ImageBuffer, ImageOutputFormat, Rgba};
 use palette::blend::Compose;
-use syntect::{parsing::{SyntaxReference, SyntaxSet}, highlighting::ThemeSet, util::LinesWithEndings};
+use syntect::{
+    highlighting::ThemeSet,
+    parsing::{SyntaxReference, SyntaxSet},
+    util::LinesWithEndings,
+};
 
 pub type PngData = Vec<u8>;
 
-pub fn draw_code(mut font_system: &mut FontSystem, swash_cache: &mut SwashCache, syntax_set: &SyntaxSet, theme_set: &ThemeSet, code: &str, syntax: &SyntaxReference) -> PngData {
+pub fn draw_code(
+    mut font_system: &mut FontSystem,
+    swash_cache: &mut SwashCache,
+    syntax_set: &SyntaxSet,
+    theme_set: &ThemeSet,
+    code: &str,
+    syntax: &SyntaxReference,
+) -> PngData {
     let metrics = Metrics::new(32.0, 44.0).scale(1.5);
     let mut buffer = Buffer::new(&mut font_system, metrics);
     let mut buffer = buffer.borrow_with(font_system);
 
-    buffer.set_size(800.0, 250.0);
+    buffer.set_size(f32::MAX, f32::MAX);
 
-    let default_text_color = Color::rgb(255,0,255); // magenta: should not appear
+    let default_text_color = Color::rgb(255, 0, 255); // magenta: should not appear
     let attrs = Attrs::new().color(default_text_color);
     let mono_attrs = attrs.family(Family::Monospace);
 
-
-    let theme = &theme_set.themes["base16-ocean.dark"];
+    let theme = &theme_set.themes["Solarized (dark)"];
+    //let theme = &theme_set.themes["base16-eighties.dark"];
     let mut h = syntect::easy::HighlightLines::new(syntax, theme);
 
     fn color_syntect_to_cosmic(c: syntect::highlighting::Color) -> cosmic_text::Color {
         cosmic_text::Color::rgba(c.r, c.g, c.b, c.a)
     }
     fn color_syntect_to_palette(c: syntect::highlighting::Color) -> palette::LinSrgba {
-        palette::LinSrgba::new(c.r as f32 / 255.0, c.g as f32 / 255.0, c.b as f32 / 255.0, c.a as f32 / 255.0)
+        palette::LinSrgba::new(
+            c.r as f32 / 255.0,
+            c.g as f32 / 255.0,
+            c.b as f32 / 255.0,
+            c.a as f32 / 255.0,
+        )
     }
 
+    buffer.lines.clear();
 
-    for line in LinesWithEndings::from(code) {
-        let ranges = h.highlight_line(line, syntax_set).expect("Failed to parse line to highlight?");
-        let line_parts = ranges.iter().map(|(style, text)| (text, mono_attrs.color(color_syntect_to_cosmic(style.foreground))));
+    for line in LinesWithEndings::from(code.trim()) {
+        let ranges = h
+            .highlight_line(line, syntax_set)
+            .expect("Failed to parse line to highlight?");
+        let line_parts = ranges.iter().map(|(style, text)| {
+            (
+                text,
+                mono_attrs.color(color_syntect_to_cosmic(style.foreground)),
+            )
+        });
         let mut attrs_list = AttrsList::new(mono_attrs);
         let mut cursor_pos = 0;
         for (text, attrs) in line_parts {
@@ -43,33 +69,57 @@ pub fn draw_code(mut font_system: &mut FontSystem, swash_cache: &mut SwashCache,
         }
 
         buffer.lines.push(BufferLine::new(line, attrs_list));
+        println!("New buffer line: {line:?}");
     }
 
-
     buffer.shape_until_scroll();
-    
-    let (wf,hf) = buffer.size();
-    let buf_width = wf.ceil() as u32;
-    let buf_height = hf.ceil() as u32;
 
-   let palette_default_pixel = color_syntect_to_palette(theme.settings.background.unwrap_or(syntect::highlighting::Color::BLACK));
+    // Figure out the size for the image based on the buffer's layout runs (laid out horizontal lines)
+    let mut max_w = 1.0f32; // So that if there is no text, it will emit a small image
+    let mut max_y = 1.0f32;
+    for run in buffer.layout_runs() {
+        max_w = max_w.max(run.line_w);
+        max_y = max_y.max(run.line_y);
+    }
 
-    let mut palette_buffer: Vec<palette::Alpha<palette::rgb::Rgb<palette::encoding::Linear<palette::encoding::Srgb>, f32>, f32>> = vec![palette_default_pixel; (buf_width * buf_height) as usize];
+    let margin = metrics.font_size as u32; // Extra space to make sure that the allocated buffer is enough to fit the entire text.
+    let buf_width = max_w.ceil() as u32 + margin;
+    let buf_height = max_y.ceil() as u32 + margin;
 
-    buffer.draw(swash_cache, default_text_color, |x,y,w,h,color| {
+    let palette_default_pixel = color_syntect_to_palette(
+        theme
+            .settings
+            .background
+            .unwrap_or(syntect::highlighting::Color::BLACK),
+    );
+
+    let mut palette_buffer: Vec<
+        palette::Alpha<
+            palette::rgb::Rgb<palette::encoding::Linear<palette::encoding::Srgb>, f32>,
+            f32,
+        >,
+    > = vec![palette_default_pixel; (buf_width * buf_height) as usize];
+
+    buffer.draw(swash_cache, default_text_color, |x, y, w, h, color| {
         let w = w as i32;
         let h = h as i32;
+        let x = x + (margin / 2) as i32;
+        let y = y + (margin / 2) as i32;
         let pixel = palette::LinSrgba::new(
-            color.r() as f32 / 255.0, color.g() as f32 / 255.0, color.b() as f32 / 255.0, color.a() as f32 / 255.0);
-        
+            color.r() as f32 / 255.0,
+            color.g() as f32 / 255.0,
+            color.b() as f32 / 255.0,
+            color.a() as f32 / 255.0,
+        );
+
         // This is an acceptable implementation of rectangle drawing,
         // because in practice most of the rectangles being drawn are 1x1 pixel.
 
-        for px in x..x+w {
-            for py in y..y+h {
+        for px in x..x + w {
+            for py in y..y + h {
                 let px = px.clamp(0, buf_width as i32) as usize;
                 let py = py.clamp(0, buf_height as i32) as usize;
-                let pos = px + py*(buf_width as usize);
+                let pos = px + py * (buf_width as usize);
 
                 let old_pixel = palette_buffer[pos];
                 palette_buffer[pos] = pixel.over(old_pixel);
@@ -77,22 +127,21 @@ pub fn draw_code(mut font_system: &mut FontSystem, swash_cache: &mut SwashCache,
         }
     });
 
-
-    let default_pixel = Rgba([255,0,255,0]);
+    let default_pixel = Rgba([255, 0, 255, 0]);
     let mut img_buffer = ImageBuffer::from_pixel(buf_width, buf_height, default_pixel);
     for (pos, value) in palette_buffer.iter().enumerate() {
         let y = pos / (buf_width as usize);
         let x = pos % (buf_width as usize);
-        let r = (value.red*255.0) as u8;
-        let g = (value.green*255.0) as u8;
-        let b = (value.blue*255.0) as u8;
-        let a = (value.alpha*255.0) as u8;
-        img_buffer.put_pixel(x as u32, y as u32, Rgba([r,g,b,a]));
+        let r = (value.red * 255.0) as u8;
+        let g = (value.green * 255.0) as u8;
+        let b = (value.blue * 255.0) as u8;
+        let a = (value.alpha * 255.0) as u8;
+        img_buffer.put_pixel(x as u32, y as u32, Rgba([r, g, b, a]));
     }
 
-
     let mut out = PngData::new();
-    img_buffer.write_to(&mut Cursor::new(&mut out), ImageOutputFormat::Png).expect("Encoding drawing into PNG in memory should be infallible");
+    img_buffer
+        .write_to(&mut Cursor::new(&mut out), ImageOutputFormat::Png)
+        .expect("Encoding drawing into PNG in memory should be infallible");
     out
-
 }
